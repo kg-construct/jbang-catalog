beec82fb5ce3fffc28cb8c43addca37f89f32306
jduchateau <jduchateau@users.noreply.github.com>
2026-07-17 14:07:14 +0000

    burp v0.1.3

---
+++ burp.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 21+
+//DEPS io.github.kg-construct:burp:0.1.3
+// Generated with JReleaser 1.12.0 at 2026-07-17T14:07:04.107873057Z
+
+public class burp {
+    public static void main(String... args) throws Exception {
+        burp.Main.main(args);
+    }
+}
