1332d687ccef8a1b807696be27657f974e10627c
Jakub Duchateau <Jakub.Duchateau@uliege.be>
2026-08-20 15:39:01 +0200

    Update burp.java

--- burp.java
+++ burp.java
@@ -1,6 +1,6 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 21+
-//DEPS io.github.kg-construct:burp:0.1.3-rc.5
+//DEPS io.github.kg-construct:burp:0.1.3
 // Generated with JReleaser 1.12.0 at 2026-07-22T09:08:20.317410749Z
 
 public class burp {
